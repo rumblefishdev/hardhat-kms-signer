baffb0da1e288b4580bab0b573600aa8b81e8cfb
Daniel Cybulka <danielcybulka@gmail.com>
2022-08-31 10:17:32 +0200

    fix pre EIP1559 tx compatibility

--- src/provider.ts
+++ src/provider.ts
@@ -1,14 +1,16 @@
-import { BigNumber, utils } from "ethers";
-import { rpcTransactionRequest } from "hardhat/internal/core/jsonrpc/types/input/transactionRequest";
-import { validateParams } from "hardhat/internal/core/jsonrpc/types/input/validation";
-import { ProviderWrapperWithChainId } from "hardhat/internal/core/providers/chainId";
-import { EIP1193Provider, RequestArguments } from "hardhat/types";
 import {
   createSignature,
   getEthAddressFromKMS,
 } from "@rumblefishdev/eth-signer-kms";
-import { keccak256 } from "ethers/lib/utils";
 import { KMS } from "aws-sdk";
+import { BigNumber, utils } from "ethers";
+import { keccak256 } from "ethers/lib/utils";
+import { rpcTransactionRequest } from "hardhat/internal/core/jsonrpc/types/input/transactionRequest";
+import { validateParams } from "hardhat/internal/core/jsonrpc/types/input/validation";
+import { ProviderWrapperWithChainId } from "hardhat/internal/core/providers/chainId";
+import { EIP1193Provider, RequestArguments } from "hardhat/types";
+
+import { toHexString } from "./utils";
 
 export class KMSSigner extends ProviderWrapperWithChainId {
   public kmsKeyId: string;
@@ -25,30 +27,32 @@ export class KMSSigner extends ProviderWrapperWithChainId {
     const method = args.method;
     const params = this._getParams(args);
     const sender = await this._getSender();
-
     if (method === "eth_sendTransaction") {
       const [txRequest] = validateParams(params, rpcTransactionRequest);
       const tx = await utils.resolveProperties(txRequest);
-      const nonce = await this._getNonce(sender);
-
+      const nonce = tx.nonce?.toNumber() ?? (await this._getNonce(sender));
       const baseTx: utils.UnsignedTransaction = {
         chainId: (await this._getChainId()) || undefined,
-        data: tx.data || undefined,
-        gasLimit: tx.gas?.toString() || undefined,
-        gasPrice: tx.gasPrice?.toString() || undefined,
-        nonce: nonce,
-        to: tx.to?.toString("hex") || undefined,
-        value: tx.value?.toString() || undefined,
-        type: undefined,
-        maxFeePerGas: tx.maxFeePerGas?.toString() || undefined,
-        maxPriorityFeePerGas: tx.maxPriorityFeePerGas?.toString() || undefined,
+        data: tx.data,
+        gasLimit: toHexString(tx.gas),
+        gasPrice: toHexString(tx.gasPrice),
+        nonce,
+        type: 2,
+        to: toHexString(tx.to),
+        value: toHexString(tx.value),
+        maxFeePerGas: tx.maxFeePerGas?.toString(),
+        maxPriorityFeePerGas: tx.maxPriorityFeePerGas?.toString(),
       };
+
       if (
-        baseTx.maxFeePerGas !== undefined &&
-        baseTx.maxPriorityFeePerGas !== undefined
+        baseTx.maxFeePerGas === undefined &&
+        baseTx.maxPriorityFeePerGas === undefined
       ) {
-        baseTx.type = 2;
+        baseTx.type = 0;
+        delete baseTx.maxFeePerGas;
+        delete baseTx.maxPriorityFeePerGas;
       }
+
       const unsignedTx = utils.serializeTransaction(baseTx);
       const hash = keccak256(utils.arrayify(unsignedTx));
       const sig = await createSignature({
